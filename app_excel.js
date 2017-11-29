21dd41d63d93ae338536d34d2bde4196964df0f0
Claudio Acioli <kllaudyo@gmail.com>
2017-11-29 08:00:12 -0200

    feat: Inicia estudo do caso, componente Excel

---
+++ app_excel.js
@@ -0,0 +1,38 @@
+var headers = ["Livro","Autores","Linguagem","Publicado em", "Vendas"];
+var data = [
+    ["Don Quixote","Miguel de Cervantes","Espanhol","1612","500 milhões"],
+    ["Um Conto de Duas Cidades","Charles Dickens","Inglês","1859","200 milhões"],
+    ["O Senhor dos Anéis","J. R. R. Tolkien","Inglês","1954–1955","150 milhões"],
+    ["O Pequeno Príncipe","Antoine de Saint-Exupéry","Francês","1943","140 milhões"],
+    ["Harry Potter e a Pedra Filosofal","J. K. Rowling","Inglês","1997","107 milhões"],
+    ["O Hobbit","J. R. R. Tolkien","Inglês","1937","100 milhões"],
+    ["O Caso dos Dez Negrinhos","Agatha Christie","Inglês","1939","100 milhões"],
+    ["O Sonho da Câmara Vermelha","Cao Xueqin","Chinês","1754–1791","100 milhões"],
+    ["Ela, a Feiticeira","H. Rider Haggard","Inglês","1887","100 milhões"]
+];
+
+var Excel = React.createClass({
+    render : function(){
+        return (
+            React.DOM.table(
+                null,
+                React.DOM.thead(
+                    null,
+                    React.DOM.tr(
+                        null,
+                        this.props.headers.map(function(title){
+                            return React.DOM.th(null, title)
+                        })
+                    )
+                ))
+        );
+    }
+});
+
+ReactDOM.render(
+    React.createElement(Excel, {
+        headers: headers,
+        initialData: data
+    }),
+    document.querySelector(".main")
+);
