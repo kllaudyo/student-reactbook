8253bc514c39f40f4a9cc6bdb29d5177eca0eb89
Claudio Acioli <kllaudyo@gmail.com>
2017-12-10 19:16:07 -0200

    feat: Adiciona componente na aplicacao

--- Whinepad v.0.0.1/js/source/app.js
+++ Whinepad v.0.0.1/js/source/app.js
@@ -3,11 +3,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Logo from './components/Logo';
+import Excel from './components/Excel';
+
+var headers = localStorage.getItem("headers");
+var data = localStorage.getItem("data");
+
+if(!headers){
+    headers = ['Title','Year','Rating','Comments'];
+    data =[['Test','2015','3','meh']];
+}
 
 ReactDOM.render(
     <h1>
-      <Logo /> Welcome to The App!
+        <Logo /> Welcome to The App!
+        <Excel headers={headers} initialData={data} />
     </h1>,
-    document.querySelector('.main')
+    document.getElementById('pad')
 );
 
