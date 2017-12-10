52d00a63f5668707cef8b107946527a315f1da36
Claudio Acioli <kllaudyo@gmail.com>
2017-12-10 13:39:53 -0200

    feat: Adiciona Whinepad

---
+++ Whinepad v.0.0.1/scripts/build.sh
@@ -0,0 +1,9 @@
+#transformação js
+babel --presets react,es2015 js/source -d js/build
+#empacotamento js
+browserify js/build/app.js -o bundle.js
+#empacotamento css
+cat css/*/* css/*.css | sed 's/..\/..\/images/images/g' > bundle.css
+#pronto
+date; echo;
+
