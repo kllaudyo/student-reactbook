74485abec0ce8f64c2459d2891bcaa95e643c14f
Claudio Acioli <kllaudyo@gmail.com>
2017-12-14 11:24:08 -0200

    feat: Adiciona componente Rating

--- whinepad/js/source/discover.js
+++ whinepad/js/source/discover.js
@@ -3,6 +3,7 @@
 import Button from './components/Button';
 import Logo from './components/Logo';
 import Suggest from './components/Suggest';
+import Rating from './components/Rating';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -13,6 +14,7 @@ ReactDOM.render(
         <div style={{display:'inline-block', background:'purple'}}>
             <Logo />
         </div>
+        <hr />
         <h2>Buttons</h2>
         <div>
             Button with onClick: <Button onClick={()=> alert('ouch')}>Click Me</Button>
@@ -23,10 +25,17 @@ ReactDOM.render(
         <div>
             Custom class name: <Button className="custom">I do nothing</Button>
         </div>
+        <hr />
         <h2>Suggest</h2>
         <div>
             <Suggest options={['eenie','meenie','miney','mo']} />
         </div>
+        <hr />
+        <h2>Ratting</h2>
+        <div>No initial value: <Rating /></div>
+        <div>Initial value 4: <Rating defaultValue={4} /></div>
+        <div>This one goes to 11: <Rating max={11} /></div>
+        <div>Read-only: <Rating readonly={true} defaultValue={3} /></div>
     </div>,
     document.querySelector('.root')
 );
