66d17c8f4f0c4a4118920a2b6ec1faa10e58ddbb
Claudio Acioli <kllaudyo@gmail.com>
2017-12-14 10:27:58 -0200

    feat: Adiciona componente Suggest

--- whinepad/js/source/discover.js
+++ whinepad/js/source/discover.js
@@ -2,6 +2,7 @@
 
 import Button from './components/Button';
 import Logo from './components/Logo';
+import Suggest from './components/Suggest';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -22,6 +23,10 @@ ReactDOM.render(
         <div>
             Custom class name: <Button className="custom">I do nothing</Button>
         </div>
-    </div>
-    ,document.querySelector('.root')
+        <h2>Suggest</h2>
+        <div>
+            <Suggest options={['eenie','meenie','miney','mo']} />
+        </div>
+    </div>,
+    document.querySelector('.root')
 );
