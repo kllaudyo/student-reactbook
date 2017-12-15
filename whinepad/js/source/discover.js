3b49cbde53a421f11ea6a25f48b0fe367ea7285f
Claudio Acioli <kllaudyo@gmail.com>
2017-12-14 23:02:07 -0200

    feat: Adiciona componente factory FormInput

--- whinepad/js/source/discover.js
+++ whinepad/js/source/discover.js
@@ -4,6 +4,7 @@ import Button from './components/Button';
 import Logo from './components/Logo';
 import Suggest from './components/Suggest';
 import Rating from './components/Rating';
+import FormInput from './components/FormInput';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -36,6 +37,36 @@ ReactDOM.render(
         <div>Initial value 4: <Rating defaultValue={4} /></div>
         <div>This one goes to 11: <Rating max={11} /></div>
         <div>Read-only: <Rating readonly={true} defaultValue={3} /></div>
+        <hr />
+        <h2>Form inputs</h2>
+        <table>
+            <tbody>
+                <tr>
+                    <td>Vanilla input</td>
+                    <td><FormInput /></td>
+                </tr>
+                <tr>
+                    <td>Prefilled</td>
+                    <td><FormInput defaultValue="it's like a default" /></td>
+                </tr>
+                <tr>
+                    <td>Year</td>
+                    <td><FormInput type="year" /></td>
+                </tr>
+                <tr>
+                    <td>Rating</td>
+                    <td><FormInput type="rating" defaultValue={4} /></td>
+                </tr>
+                <tr>
+                    <td>Suggest</td>
+                    <td><FormInput type="suggest" options={['red','green','blue']} defaultValue="green" /></td>
+                </tr>
+                <tr>
+                    <td>Vanilla textarea</td>
+                    <td><FormInput type="text" /></td>
+                </tr>
+            </tbody>
+        </table>
     </div>,
     document.querySelector('.root')
 );
