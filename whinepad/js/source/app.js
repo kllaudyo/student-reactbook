136d3e5a4932dd4941344f8b2a18a69befe91728
Claudio Acioli <kllaudyo@gmail.com>
2017-12-18 23:05:49 -0200

    refactor: Altera app principal

--- whinepad/js/source/app.js
+++ whinepad/js/source/app.js
@@ -2,22 +2,25 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ExcelOne from './components/ExcelOne';
 import Logo from './components/Logo';
-import Excel from './components/Excel';
+import schema from './schema';
 
-var headers = localStorage.getItem("headers");
-var data = localStorage.getItem("data");
 
-if(!headers){
-    headers = ['Title','Year','Rating','Comments'];
-    data =[['Test','2015','3','meh']];
+var data = JSON.parse(localStorage.getItem("data"));
+if(!data){
+    data = {};
+    schema.forEach(item=> data[item.id] = item.sample);
+    data = [data];
 }
 
 ReactDOM.render(
-    <h1>
-        <Logo /> Welcome to The App!
-        <Excel headers={headers} initialData={data} />
-    </h1>,
+    <div>
+        <div className="app-header">
+            <Logo /> Welcome to Whinepad!
+        </div>
+        <ExcelOne initialData={data} schema={schema} />
+    </div>,
     document.getElementById('pad')
 );
 
