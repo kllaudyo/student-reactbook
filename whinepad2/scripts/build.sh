2475a8f57433a604c91d77ecad9f3ef79636c446
Claudio Acioli <kllaudyo@gmail.com>
2017-12-27 09:53:23 -0200

    chore: Configura babel no package json eusa no script

--- whinepad2/scripts/build.sh
+++ whinepad2/scripts/build.sh
@@ -1,5 +1,5 @@
 #transformação js
-babel --presets react,es2015 js/source -d js/build
+babel js/source -d js/build
 #empacotamento js
 browserify js/build/app.js -o bundle.js
 browserify js/build/discover.js -o discover-bundle.js
