e0e8d1acc64993e8675e43f4b2b82a840abe0e7d
Claudio Acioli <kllaudyo@gmail.com>
2018-01-03 21:09:33 -0200

    test: Adiciona teste para componente Button

---
+++ whinepad2/js/__tests__/testMyButton.js
@@ -0,0 +1,19 @@
+jest
+    .dontMock('../source/components/Button')
+    .dontMock('classnames');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
+import Button from '../source/components/Button';
+
+describe("Renderiza componente Button", () => {
+    it("renderiza <a> vs <button>", () => {
+        const button = ReactTestUtils.renderIntoDocument(
+            <div>
+                <Button>Hello</Button>
+            </div>
+        );
+        expect(ReactDOM.findDOMNode(button).children[0].nodeName).toEqual('BUTTON');
+    });
+});
