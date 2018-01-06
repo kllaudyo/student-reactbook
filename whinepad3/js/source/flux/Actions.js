24524c3d6cf8939f4644b82de063cb9536a1ccbb
Claudio Acioli <kllaudyo@gmail.com>
2018-01-06 07:55:55 -0200

    feat: Implementa metodos CRUD Action

--- whinepad3/js/source/flux/Actions.js
+++ whinepad3/js/source/flux/Actions.js
@@ -1,7 +1,26 @@
 import Store from "./Store";
 
 const CRUDActions = {
-    /*Métodos*/
+    create(newRecord){
+        let data = Store.getData();
+        data.unshift(newRecord);
+        Store.setData(data);
+    },
+    delete(recordId){
+        let data = Store.getData();
+        data.splice(recordId,1);
+        Store.setData(data);
+    },
+    updateRecord(recordId, newRecord){
+        let data = Store.getData();
+        data[recordId] = newRecord;
+        Store.setData(data);
+    },
+    updateField(recordId, key, value){
+        let data = Store.getData();
+        data[recordId][key] = value;
+        Store.setData(data);
+    }
 };
 
 export default CRUDActions;
